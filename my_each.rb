7adfe14782dc06e4ecbfa0e32d09a6296425fdd2
Trevor Seitz <trevinla@gmail.com>
2018-03-13 16:27:21 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 require "pry"
-def my_each (array)
+def my_each (array) do |item|
   counter = 0
-  while counter <= array.length + 1
-    array[counter]
+  while counter <= array.length
+    # array[counter]
+    item
     # binding.pry
     counter += 1
   # end
